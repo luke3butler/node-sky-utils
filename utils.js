70a9e5f14b4e2d7d3f94d6da865acbf5817b4951
Luke Butler <luke3butler+github@gmail.com>
2017-04-05 22:42:49 -0400

    Improve type checking and add type checking methods

--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // Function to assign defaults for missing values
 // Overwrite arrays, merge objects, always favor obj
@@ -18,21 +18,32 @@ const setDefaults = (obj, defaults) => {
 };
 
 // Returns true for objects and arrays
-const typeofObject = item => item !== null && typeof item === "object";
+const typeofObject = item => item !== null && typeof item === 'object';
 
 // Returns true for arrays, but not objects
 const isArray = item => Array.isArray(item);
 
 // Returns true for objects, but not arrays
-const isObject = item => typeofObject(item) && !isArray(item);
+const isObject = item => classIs('Object', item);
 
 // Returns true if node is running on macOS
-const isMacOS = () => "darwin" === process.platform;
+const isMacOS = () => 'darwin' === process.platform;
+
+// Returns true if defined and not null
+const notEmpty = item => item !== undefined && item !== null;
+
+// Returns the class - https://bonsaiden.github.io/JavaScript-Garden/#types
+const classType = item => Object.prototype.toString.call(item).slice(8, -1);
+
+// Check the class of an item
+const classIs = (type, item) => notEmpty(item) && classType(item) === type;
 
 module.exports = {
   setDefaults,
   isMacOS,
   typeofObject,
   isArray,
-  isObject
+  isObject,
+  classType,
+  classIs,
 };
