37eaabf5180f2a392d9c1a809f4b612fee638888
linsk1998 <linsk@live.com>
2021-03-02 10:37:58 +0800

    add browser version

---
+++ browser-utils.js
@@ -0,0 +1,7 @@
+
+export * from "./universal/setDefaults";
+export * from "./universal/typeofObject";
+export * from "./universal/isArray";
+export * from "./universal/isObject";
+export * from "./universal/classType";
+export * from "./universal/classIs";
---
+++ build/rollup.config.browser.js
@@ -0,0 +1,8 @@
+export default {
+	input: 'browser-utils.js',
+	output: {
+		file: 'dist/browser.js',
+		format: 'esm'
+	},
+	context: 'global'
+}
